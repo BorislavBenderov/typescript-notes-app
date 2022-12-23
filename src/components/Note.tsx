7b0b81a4e5dfcacea0dc58b014fa573f4ca0a6a3
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-23 13:41:45 +0200

    Add Note component

---
+++ src/components/Note.tsx
@@ -0,0 +1,32 @@
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useNote } from "./NoteLayout";
+
+export const Note = () => {
+    const note = useNote();
+    return (
+        <>
+            <Row className="align-items-center mb-4">
+                <Col>
+                    <h1>{note.title}</h1>
+                    {note.tags.length > 0 && (
+                        <Stack gap={1} direction='horizontal' className='flex-wrap'>
+                            {note.tags.map(tag => (
+                                <Badge className='text-truncate' key={tag.id}>{tag.label}</Badge>
+                            ))}
+                        </Stack>
+                    )}
+                </Col>
+                <Col xs='auto'>
+                    <Stack gap={2} direction='horizontal'>
+                        <Link to={`/${note.id}/edit`}>
+                            <Button variant='primary'>Edit</Button>
+                        </Link>
+                        <Button variant='outline-danger'>Delete</Button>
+                        <Link to='/'><Button variant='outline-secondary'>Back</Button></Link>
+                    </Stack>
+                </Col>
+            </Row>
+        </>
+    );
+}
