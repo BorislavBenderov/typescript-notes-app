6627b5e07f5f2b979697c3ebc87df28007622d7e
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-23 13:41:34 +0200

    Add NoteLayout component

---
+++ src/components/NoteLayout.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet, useOutletContext, useParams } from "react-router-dom";
+import { Note } from "../App";
+
+type NoteLayoutProps = {
+    notes: Note[]
+}
+
+export const NoteLayout = ({ notes }: NoteLayoutProps) => {
+    const { id } = useParams();
+    const note = notes.find(n => n.id === id);
+
+    if (note === null) return <Navigate to='/' replace />
+
+    return <Outlet context={note}/>
+}
+
+export const useNote = () => {
+    return useOutletContext<Note>();
+}
