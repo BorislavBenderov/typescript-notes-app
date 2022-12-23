590f068674c7cdfdf2d5a8761a681cc5924d231c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-23 14:40:46 +0200

    Add delete button

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,9 +1,16 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import { Link, useNavigate } from "react-router-dom";
 import { useNote } from "./NoteLayout";
 
-export const Note = () => {
+type NoteProps = {
+    onDelete: (id: string) => void
+}
+
+export const Note = ({ onDelete }: NoteProps) => {
     const note = useNote();
+    const navigate = useNavigate();
+
     return (
         <>
             <Row className="align-items-center mb-4">
@@ -22,11 +29,15 @@ export const Note = () => {
                         <Link to={`/${note.id}/edit`}>
                             <Button variant='primary'>Edit</Button>
                         </Link>
-                        <Button variant='outline-danger'>Delete</Button>
+                        <Button onClick={() => {
+                            onDelete(note.id)
+                            navigate('/')
+                        }} variant='outline-danger'>Delete</Button>
                         <Link to='/'><Button variant='outline-secondary'>Back</Button></Link>
                     </Stack>
                 </Col>
             </Row>
+            <ReactMarkdown>{note.markdown}</ReactMarkdown>
         </>
     );
 }
