e9b658688472c8b373ffa74fae7f868b6c1de0c0
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-23 11:17:34 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { NewNote } from './components/NewNote';
+import { useLocalStorage } from './hooks/useLocalStorage';
+import { useMemo } from 'react';
+
+
 function App() {
+  
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/new' element={<NewNote />} />
+        <Route path='*' element={<Navigate to='/' />} />
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+        </Route>
+      </Routes>
+    </Container>
   );
 }
 
